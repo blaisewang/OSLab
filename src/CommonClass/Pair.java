eb1b2875d14964bc1091e3bc0f640d59672cc738
blaisewang <kaito.kaito.k@gmail.com>
2017-04-23 00:48:43 +0800

    Serializable

--- src/CommonClass/Pair.java
+++ src/CommonClass/Pair.java
@@ -1,11 +1,15 @@
 package CommonClass;
 
+import java.io.Serializable;
+
 /**
  * CommonClass
  * Created by blaisewang on 2016/7/7.
  */
 
-public class Pair<A extends Comparable<A>, B> implements Comparable<Pair<A, B>> {
+public class Pair<A extends Comparable<A>, B> implements Comparable<Pair<A, B>>, Serializable {
+    private static final long serialVersionUID = -8914647164831651005L;
+
     public A first;
     public B second;
 
